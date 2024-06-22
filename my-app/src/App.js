2d620213b2ff4a4b21aa1e6a0ed6373f4f7538fe
photo2story <photo2story@naver.com>
2024-06-22 23:20:51 +0900

    png

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,25 +2,44 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const App = () => {
+  const [stockName, setStockName] = useState('');
   const [imageUrl, setImageUrl] = useState('');
 
-  useEffect(() => {
-    loadImage();
-  }, []);
+  const handleInputChange = (event) => {
+    setStockName(event.target.value.toUpperCase());
+  };
 
-  const loadImage = async () => {
+  const handleSearch = async () => {
     try {
-      const url = 'https://my-buddy-app-355192a036b3.herokuapp.com/image/comparison_AAPL_VOO.png';
-      setImageUrl(url);
+      const response = await axios.get(`/image/comparison_${stockName}_VOO.png`);
+      setImageUrl(response.config.url);
     } catch (error) {
       console.error('Error fetching image:', error);
+      alert('이미지를 찾을 수 없습니다. 다시 시도해 주세요.');
     }
   };
 
   return (
     <div className="App">
       <h1>React와 Flask 연동</h1>
-      {imageUrl && <img src={imageUrl} alt="Stock Comparison" style={{ width: '100%' }} />}
+      <input 
+        type="text" 
+        id="stockName" 
+        value={stockName}
+        onChange={handleInputChange}
+        placeholder="Enter stock name"
+      />
+      <button id="searchReviewButton" onClick={handleSearch}>Search</button>
+      {imageUrl && (
+        <div>
+          <h3>{stockName} vs VOO</h3>
+          <img 
+            src={imageUrl} 
+            alt={`${stockName} vs VOO`} 
+            style={{ width: '100%' }} 
+          />
+        </div>
+      )}
     </div>
   );
 }
