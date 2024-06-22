c8b6165baca094ea4cb589a20ff1768f34bd9a0d
photo2story <photo2story@naver.com>
2024-06-22 19:05:30 +0900

    app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
 
 function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch('http://127.0.0.1:5000/api/data')
+    fetch('http://127.0.0.1:5000/api/data') // 백엔드 API 엔드포인트
       .then(response => response.json())
-      .then(data => setData(data.message))
-      .catch(error => console.error('Error fetching data:', error));
+      .then(data => setData(data));
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          {data ? data : 'Loading...'}
-        </p>
+        <h1>React와 Flask 연동</h1>
+        {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading...</p>}
       </header>
     </div>
   );
