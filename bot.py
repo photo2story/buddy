a0214371175f7f00352abb15800320c5b3e28dcc
photo2story <photo2story@naver.com>
2024-06-22 20:42:48 +0900

    git add . git commit -m "Fix Discord bot intents for latest discord.py version" git push heroku main

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
-import asyncio
 import certifi
 
 # Load environment variables
@@ -17,8 +16,9 @@ CHANNEL_ID = os.getenv('DISCORD_CHANNEL_ID')
 # SSL certificate verification using certifi
 os.environ['SSL_CERT_FILE'] = certifi.where()
 
-intents = discord.Intents.all()
-intents.message_content = True
+intents = discord.Intents.default()
+intents.messages = True
+
 client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='', intents=intents)
 
@@ -36,6 +36,6 @@ async def ping(ctx):
     print(f"Ping command received from {ctx.author.name}")
     await ctx.send(f'pong: {bot.user.name}')
 
-# 여기에 필요한 다른 봇 명령어와 기능 추가
+# Add other bot commands and functionalities as needed
 
 bot.run(TOKEN)
